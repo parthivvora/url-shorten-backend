a62b8f6c4035c5f9b053838f07d82f570058e277
parthivvora <v.parthiv004@gmail.com>
2025-03-30 14:08:36 +0530

    Update CORS configuration to allow requests from localhost during development

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ require("./config/db").connectToDb();
 
 app.use(
   cors({
-    origin: "https://url-shorten-frontend-pi.vercel.app",
+    origin: [
+      "https://url-shorten-frontend-pi.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
